bfc79a9f8afcf64c7483e14a8af111a9b64380e5
Matheus Santos <96271822+Mattheus-Santos@users.noreply.github.com>
2025-01-15 21:07:28 -0300

    Update des.056.py

--- des.056.py
+++ des.056.py
@@ -7,23 +7,22 @@ homem_maisvelho_idade = 0
 
 
 for i in range(1, 5):
+    print(f"------{i}ª pessoa.------")
     nome = str(input("Digite o nome: "))
     idade = int(input("Digite a idade: "))
-    sexo = str(input("Sexo [M/F]: "))
+    sexo = str(input("Sexo [M/F]: ")).strip()
 
     soma_idade += idade
-    if sexo == 'M'and idade > homem_maisvelho_idade:
+    if sexo in 'M'and idade > homem_maisvelho_idade:
         homem_maisvelho_nome = nome
         homem_maisvelho_idade = idade
-    elif sexo == 'F' and idade < 20:
+    elif sexo in 'F' and idade < 20:
         contador_de_mulheres_jovens += 1
 
-    print(f"------{i}ª pessoa.------")
-    print(f"Nome:{nome}")
-    print(f"Idade:{idade}")
-    print(f"Sexo:{sexo}")
+    
+    
 
 media_idade = soma_idade / 4
 print(f"A média de idade é: {media_idade}")
-print(f"O homem mais velhor é: {homem_maisvelho_nome}")
+print(f"O homem mais velhor é: {homem_maisvelho_nome} e se possui: {homem_maisvelho_idade} anos.")
 print(f"{contador_de_mulheres_jovens} mulheres são menores de 20 anos!")
